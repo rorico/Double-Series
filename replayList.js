c2973a89265ab93dd0aa0391b5aa88bf775056c2
DANIEL CHEN <dlchen@uwaterloo.ca>
2017-06-28 08:44:49 -0400

    fix replaylist not showing first game
    TIL what reduce does

--- replayList.js
+++ replayList.js
@@ -2,10 +2,11 @@
 $.ajax({
     url: "replayList.json",
     success: function(games) {
-        games.reduce(function(a,val) {
+        for (var i = 0 ; i < games.length ; i++) {
+            val = games[i];
             var file = val;
             var esc = file.replace(/'/g,"&#39;").replace(/"/g,"&quot;");
             $("body").append("<ul><a href='replay?games/" + esc + "'>" + file + "</a></ul>");
-        })
+        }
     }
 });
