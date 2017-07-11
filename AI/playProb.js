00bbedff5ee88ec5c0955288a0c143e31d91d8fe
DANIEL CHEN <dlchen@uwaterloo.ca>
2017-07-11 09:09:44 -0400

    first version of playProb
    commit before for this was accidental
    still needs cleaning up and work

--- AI/playProb.js
+++ AI/playProb.js
@@ -18,9 +18,15 @@ var playProb = module.exports = function(info) {
     var offensive = true;
     var me = info.player;
     var team = info.team;
+    var hand = info.hand;
     var board = info.board;
     var points = info.points;
 
+    var bestLines = [];
+    var turnsToFinish = Infinity;
+    var linesPartialDone = [];
+
+    var teams = [1,3]; //list of teams, may change in future
     var possible = [];
     var references = [];
     var cardsLeft = [];
@@ -28,40 +34,62 @@ var playProb = module.exports = function(info) {
     var numPlusJ;
     var numMinusJ;
 
-    var movesLeft = [Infinity,Infinity];
+    var turnsToWin = [];
 
     probSetup();
+    //game could already be started
+    if (hand) {
+        onNewGame(info);
+    }
 
     return {
         play:playProb,
-        setup:setup,
         onPlay:onPlay,
         onNewGame:onNewGame
-    }
+    };
 
     function onPlay(play) {
-        console.log(play);
+        //console.log(play);
         cardsLeft[play.card]--;
         totCardsLeft--;
-        if (play.action === constants.PLAY_FINISH) {
-            for (var i = 0 ; i < player.finishedLines.length ; i++) {
-                updatePosition(player.finishedLines[i],play.action);
+        if (play.player === me) {
+            cardsLeft[play.newCard]--;
+            if (play.newCard === 0) {
+                numPlusJ++;
             }
-        } else if (play.action === constants.PLAY_REPLACE) {
-            var cardOptions
-            for (var i = 0 ; i < player.finishedLines.length ; i++) {
-                updatePosition(player.finishedLines[i],play.action);
+            if (play.cardPlayed === 0) {
+                numPlusJ--;
             }
+        }
+        updatePlay(play);
+    }
 
+    function updatePlay(play) {
+        if (play.action === constants.PLAY_FINISH) {
+            for (var i = 0 ; i < play.finishedLines.length ; i++) {
+                var line = play.finishedLines[i];
+                for (var j = 0 ; j < line.length ; j++) {
+                    updatePosition(line[j],play.action,play.team);
+                }
+            }
+        }
+        //update everywhere that the card could be used
+        var card = play.cardPlayed;
+        if (card === 0 || card === -1) {
+            updatePosition(play.position,play.action,play.team);
         } else {
-            updatePosition(play.position,play.action);
+            var options = cardOptions(play.cardPlayed);
+            for (var i = 0 ; i < options.length ; i++) {
+                updatePosition(options[i],play.action,play.team);
+            }
         }
     }
 
     function onNewGame(info) {
-        var hand = info.hand;
+        //console.log(info)
+        hand = info.hand;
         //starting at higher # is slightly faster
-        for (var i = 49 ; i >= 2 ; i++) {
+        for (var i = 49 ; i >= 2 ; i--) {
             cardsLeft[i] = 2;
         }
         cardsLeft[1] = 4;
@@ -72,62 +100,225 @@ var playProb = module.exports = function(info) {
             var card = hand[i];
             cardsLeft[card]--;
             totCardsLeft--;
+            if (card === 0) {
+                numPlusJ++;
+            }
+        }
 
-            //calculate some stuff here for these cards
+        for (var i = 0 ; i < teams.length ; i++) {
+            var team = teams[i];
+            linesPartialDone[team] = [];
+            turnsToWin[team] = Infinity;
+            for (var j = 0 ; j < possible.length ; j++) {
+                var p = possible[j];
+                p.turnsToFinish[team] = Infinity;
+                p.cardsToFinish[team] = 5;
+            }
+        }
+        //console.log(me,linesPartialDone);
+    }
+
+    function updatePosition(pos,action,team) {
+        //console.log("update")
+        //remeber to update team
+        var x = pos[0];
+        var y = pos[1];
+        var card = board[x][y];
+        var refs = references[x][y];
+                //console.log("ref",refs)
+        for (var i = 0 ; i < teams.length ; i++) {
+            var curTeam = teams[i];
+            //console.log("team",curTeam);
+            //what the place has been turned into
+            for (var j = 0 ; j < refs.length ; j++) {
+                var line = possible[refs[j]];
+                calcTurnsToFinish(line,curTeam);
+            }
+            var turnsToWinParts = [];
+            var partial = linesPartialDone[curTeam];
+            for (var j = 0 ; j < partial.length ; j++) {
+                var cardsNeeded = j;
+                var totCards = totCardsLeft;
+                if (curTeam === team) {
+                    cardsNeeded -= numPlusJ;
+                } else {
+                    //totCards - 7;
+                }
+                var thusPartial = partial[j];
+                //console.log("thusPartial[",j,"]",thusPartial)
+                turnsToWinParts.push(thusPartial ? thusPartial * Math.pow(totCardsLeft,j) : Infinity);
+            }
+            //console.log("turns to win", turnsToWinParts)
+            turnsToWin[curTeam] = addTurnsToWin.apply(null,turnsToWinParts);
+            //console.log(turnsToWin)
         }
     }
 
-    function updatePosition(pos,action) {
-        var x = pos[0];
-        var y = pos[1];
-        //what the place has been turned into
-        for (var i = 0 ; i < references[x][y] ; i++) {
-            var lineProb = references[x][y];
-            switch (action) {
-                case constants.PLAY_FINISH:
-                case constants.PLAY_FINISH:
+    function calcTurnsToFinish(line,curTeam) {
+        //console.log("line",me,curTeam,team)
+        //this can be optimized
+        var start = line.position;
+        var dir = line.direction;
+
+        var turnsToFinish = 0;
+        var cardsToFinish = 0;
+        for (var j = 0 ; j < 5 ; j++) {
+            var x = start[0] + j*dir[0];
+            var y = start[1] + j*dir[1];
+            var point = points[x][y];
+            if (point === 0) {
+                //calculate below
+            } else if (point === curTeam) {
+                continue;
+            } else if (point % 2 === 1) {
+                //even means its completed
+                cardsToFinish++;
+                //console.log("test",x,y)
+                turnsToFinish += turns(-1);
+            } else {
+                turnsToFinish = Infinity;
+                break;
             }
+            var cardNeeded = board[x][y];
+            cardsToFinish++;
+            turnsToFinish += turns(cardNeeded);
         }
-        switch (points[x][y]) {
-            case 1:
-            case 0:
+
+        var partial = linesPartialDone[curTeam];
+        //console.log("calc",team,linesPartialDone,partial,cardsToFinish,turnsToFinish)
+        //return {cards:cardsToFinish:turns:turnsToFinish};
+        var prevCards = line.cardsToFinish[curTeam];
+
+        partial[prevCards] = changeTurnsToWin(partial[prevCards] || Infinity,line.turnsToFinish[curTeam],Infinity);
+        partial[cardsToFinish] = changeTurnsToWin(partial[cardsToFinish] || Infinity,Infinity,turnsToFinish);
+        //console.log("partial",partial,prevCards,cardsToFinish)
+
+        line.cardsToFinish[curTeam] = cardsToFinish;
+        line.turnsToFinish[curTeam] = turnsToFinish;
+
+        function turnsToPickUp(card) {
+            //i'm not sure if this is accurate, but it will do for now
+            var cards = cardsLeft[card];
+            if (cards) {
+                // this number depends on how many cards there are remaining, but this changes, and is optimized out
+                return 1 / (cards + 1);
+            }
+            return Infinity;
+        }
+
+        function turns(card) {
+            return team === curTeam ? turnsME(card) : turnsThem(card);
+        }
+
+        function turnsME(card) {
+            return inHand(card) ? 1 : turnsToPickUp(card);
+        }
+
+        function turnsThem(card) {
+            return turnsToPickUp(card);
+        }
+
+        function inHand(card) {
+            for (var i = 0 ; i < hand.length ; i++) {
+                if (hand[i] === card) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    //turns add like resistors - reciprocal of sum of reciprocals
+    function addTurnsToWin() {
+        var sum = 0;
+        for (var i = 0 ; i < arguments.length ; i++) {
+            sum += 1/arguments[i];
+        }
+        return 1/sum;
+    }
+
+    function changeTurnsToWin(total,old,n) {
+        return 1/(1/total - 1/old + 1/n);
+    }
+
+    function calcTurnsToWin() {
+        for (var i = 0 ; i < possible.length ; i++) {
+            for (var j = 0 ; j < possible.length ; j++) {
+                return;
+            }
         }
     }
 
     function playProb(hand,team,info) {
-
+//console.log("TEST")
         //update probabilities
         //get best probability
-        var max = 0;
+        var min = Infinity;
+        var minCard;
+        var minPos;
+        var minAction;
 
+        //mock the play, then see the difference, revert
         for (var i = 0 ; i < hand.length ; i++) {
             var card = hand[i];
-            if (card === 0 || card === -1) {
-
+            var options;
+            var to;
+            var start;
+            var fix;
+            if (card === -1) {
+                options = removeJoptions();
+                to = 0;
+                start = constants.PLAY_REMOVE;
+                fix = constants.PLAY_ADD;
+            } else if (card === 0) {
+                options = addJoptions();
+                to = team;
+                start = constants.PLAY_ADD;
+                fix = constants.PLAY_REMOVE;
+                numPlusJ--;
             } else {
-                var lines = references[card];
-                for (var j = 0 ; j < lines.length ; j++) {
-                    var line = lines[j];
+                options = cardOptions(card);
+                to = team;
+                start = constants.PLAY_ADD;
+                fix = constants.PLAY_REMOVE;
+            }
+//console.log("options",options)
+
+            for (var k = 0 ; k < options.length ; k++) {
+                var option = options[k];
+                var fixTo = points[option[0]][option[1]];
+                points[option[0]][option[1]] = to;
+
+                //remember to check for finishline stuff
+                updatePosition(option,start,team);
+                var turns = turnsToWin[team];
+                points[option[0]][option[1]] = fixTo;
+                updatePosition(option,fix,team);
+//console.log("turns",turns)
+                if (turns < min) {
+                    min = turns;
+                    minCard = i; //index of card
+                    minPos = option;
+                    minAction = start;
                 }
             }
+            if (card === 0) {
+                numPlusJ++;
+            }
         }
+        return {action:minAction,card:minCard,position:minPos};
+    }
 
-
-
-                    var info = {
-                        position:[x,y],
-                        direction:[dirX,dirY],
-                        probabilities:[1,1],
-                        done:[0,0],
-                        //to be filled in later
-                        collisions:[]
-                    };
-
-
-        for (var i = 0 ; i < possible.length ; i++) {
-
+    function turnsToPickUp(card) {
+        //i'm not sure if this is accurate, but it will do for now
+        var cards = cardsLeft[card];
+        if (cards) {
+            var cardsInOthers = 7*3;
+            var cardsWillDraw = Math.floor(totCardsLeft/4);
+            var otherCards = totCardsLeft - cardsWillDraw + cardsInOthers;
+            return (totCardsLeft + cardsInOthers) / (cards + 1);
         }
-        
+        return Infinity;
     }
 
     function probSetup() {
@@ -138,51 +329,37 @@ var playProb = module.exports = function(info) {
         for (var i = 0 ; i < 10 ; i++) {
             var temp = [];
             for (var j = 0 ; j < 10 ; j++) {
-                var temp.push([]);
+                temp.push([]);
             }
             references.push(temp);
         }
         for (var i = 0 ; i < possible.length ; i++) {
             var p = possible[i];
-            var x = p[0][0];
-            var y = p[0][1];
-            var dirX = p[1][0];
-            var dirY = p[1][1];
+            var x = p.position[0];
+            var y = p.position[1];
+            var dirX = p.direction[0];
+            var dirY = p.direction[1];
 
             for (var j = 0 ; j < 4 ; j++) {
                 references[x + j*dirX][y + j*dirY].push(i);
             }
         }
-        /*for (var i = 0 ; i < possible.length ; i++) {
-            for (var j = i + 1 ; j < possible.length ; j++) {
-                var meet = true;
-                var meetPoint;
-                if (sameDir(possible[i],possible[j])) {
-                    var p1 = possible[i];
-                    var p2 = possible[j];
-                    if (dirX) {
-                        var x1 = p1[0][0];
-                        var x2 = p2[0][0];
-                        if ((x1 > x2 && x1 + 5 < x2) || (x2 > x1 && x2 + 5 < x1)) {
-                            meet = true;
-                        } else {
-                            meet = false;
-                        }
-                    }
-                    if (p1[0][0] === )
-                } else {
 
+        function sharePoints(line1,line2) {
+            var cnt = 0;
+            for (var i = 0 ; i < 4 ; i++) {
+                var x1 = line1.position[0] + i*line1.direction[0];
+                var y1 = line1.position[1] + i*line1.direction[1];
+                for (var j = 0 ; j < 4 ; j++) {
+                    var x2 = line2.position[0] + i*line2.direction[0];
+                    var y2 = line2.position[1] + i*line2.direction[1];
+                    if (x1===x2 && y1===y2) {
+                        cnt++;
+                    }
                 }
             }
-        }*/
-    }
-
-    function sameDir(dir1,dir2) {
-        return dir1[0] === dir2[0] && dir1[1] === dir2[1];
-    }
-
-    function isDiag(dir) {
-        return dirX && Math.abs(dirY);
+            return cnt;
+        }
     }
 
     function start(dirX,dirY) {
@@ -193,10 +370,11 @@ var playProb = module.exports = function(info) {
                     var info = {
                         position:[x,y],
                         direction:[dirX,dirY],
-                        probabilities:[1,1],
-                        done:[0,0],
                         //to be filled in later
-                        collisions:[]
+                        collisions:[],
+                        turnsToFinish:[],
+                        cardsToFinish:[]
+                        //this will hold other stuff
                     };
                     possible.push(info);
                 }
@@ -219,4 +397,8 @@ var playProb = module.exports = function(info) {
         }
         return possible;
     }
-}
+
+    function outOfBounds(x,y) {
+        return x > 9 || x < 0 || y > 9 || y < 0;
+    }
+};
